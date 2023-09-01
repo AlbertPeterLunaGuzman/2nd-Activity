ff11237bb12a03d94a487260abcb76b9e450f885
AlbertPeterLunaGuzman <127205150+AlbertPeterLunaGuzman@users.noreply.github.com>
2023-09-01 14:57:09 +0800

    Add files via upload

---
+++ studentNames/src/main/java/com/mycompany/studentnames/sorter.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.studentnames;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Albert
+ */
+public class sorter extends StudentNames{
+    String[] array;
+    String name;
+    
+    public sorter (String[] array){
+        String[] nameCollection = studentNameCollections(array);
+        
+        mySorter(nameCollection);
+    }
+    
+    private String[] studentNameCollections(String[] array){
+        return this.array = array;
+    }
+    private void mySorter(String[] array){
+        Arrays.sort(array);
+        System.out.println("\nThe sorted Array: ");
+        for(String names : array)
+            System.out.println(names);
+        
+    }
+}
